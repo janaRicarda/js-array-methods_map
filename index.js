d2cf551eb6e7d5a2105ac4f62668147472d81a24
Jana May <jana.ricarda.may@gmail.com>
2024-01-17 11:22:59 +0100

    solve challenge

--- index.js
+++ index.js
@@ -24,10 +24,16 @@ const cards = [
   },
 ];
 
-const lowerCaseAnswers = null; // ['as often as you like.', ...]
+const lowerCaseAnswers = cards.map((card) => {
+  return card.answer.toLowerCase();
+});
 
-const questionsAndAnswersTogether = null; // ["How often can I use <header>? - As often as you like.", ...]
+const questionsAndAnswersTogether = cards.map((card) => {
+  return card.question + " - " + card.answer;
+});
 
-const questionAndAnswer = null; // [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
+const questionAndAnswer = cards.map((card) => {
+  return "question: " + card.question + " answer: " + card.asnwer;
+});
 
 export { lowerCaseAnswers, questionsAndAnswersTogether, questionAndAnswer };
